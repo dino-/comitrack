053107cfee2cf01d9568fc70643ebd6f010d643a
Dino Morelli <dino@ui3.info>
2018-03-01 15:56:41 -0500

    Formatting the modified time into a better format

--- src/Handler/SeriesList.hs
+++ src/Handler/SeriesList.hs
@@ -4,28 +4,31 @@
 
 module Handler.SeriesList where
 
+import Data.Time.LocalTime ( TimeZone, getCurrentTimeZone )
 import Yesod.Table ( Table )
 import qualified Yesod.Table as Table
 
 import Import
+import Model.Series ( formatModifiedDate )
 
 
-seriesTable :: Table site Series
-seriesTable = mempty
+seriesTable :: TimeZone -> Table site Series
+seriesTable localTimeZone = mempty
   <> Table.text   "Title"               seriesTitle
   <> Table.text   "Source"              (maybe "" id . seriesSourceName)
   <> Table.text   "Publication status"  (tshow . seriesPubStatus)
   <> Table.text   "Reading status"      (tshow . seriesReadingStatus)
   <> Table.int    "Issues read"         seriesIssuesRead
-  <> Table.text   "Last modified"       (tshow . seriesModified)
+  <> Table.text   "Last modified"       (formatModifiedDate localTimeZone . seriesModified)
 
 
 getSeriesListR :: Handler Html
 getSeriesListR = do
+  localTimeZone <- liftIO getCurrentTimeZone
   series <- runDB $ map entityVal <$> selectList [] [Asc SeriesFileAsTitle]
   defaultLayout
     [whamlet|
       <p>
       <a href="@{SeriesAddR}" class="btn btn-primary active">Add a new series
-      ^{Table.buildBootstrap seriesTable series}
+      ^{Table.buildBootstrap (seriesTable localTimeZone) series}
     |]
--- src/Model/Series.hs
+++ src/Model/Series.hs
@@ -1,15 +1,20 @@
+{-# LANGUAGE NoImplicitPrelude          #-}
 {-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE TemplateHaskell            #-}
 
 module Model.Series
   ( PubStatus (..)
   , ReadingStatus (..)
+  , formatModifiedDate
   , pubStatusOptionPairs
   , readingStatusOptionPairs
   )
   where
 
 import ClassyPrelude.Yesod
+import Data.String.Conv ( toS )
+import Data.Time.Format ( defaultTimeLocale, formatTime )
+import Data.Time.LocalTime ( TimeZone, utcToZonedTime )
 
 
 data PubStatus = InProduction | Ended
@@ -28,3 +33,8 @@ derivePersistField "ReadingStatus"
 
 readingStatusOptionPairs :: [(Text, ReadingStatus)]
 readingStatusOptionPairs = [("Reading", Reading), ("Not reading", NotReading)]
+
+
+formatModifiedDate :: TimeZone -> UTCTime -> Text
+formatModifiedDate z u = toS . formatTime defaultTimeLocale "%F %R"
+  $ utcToZonedTime z u
