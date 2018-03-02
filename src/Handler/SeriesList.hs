cbb8d935ac82b25f2ed4cfae773fca97864f589b
Dino Morelli <dino@ui3.info>
2018-03-02 16:38:57 -0500

    Source table cells are now a visitable <a> tag, if applicable

--- src/Handler/SeriesList.hs
+++ src/Handler/SeriesList.hs
@@ -5,9 +5,9 @@
 module Handler.SeriesList where
 
 import Data.Time.LocalTime ( TimeZone, getCurrentTimeZone )
-import Colonnade
+import Colonnade ( Colonnade, Headed, headed )
 import qualified Text.Blaze.Html5.Attributes as HA
-import Yesod.Colonnade
+import Yesod.Colonnade ( Cell, cell, encodeCellTable, textCell )
 
 import Import
 import Model.Series ( formatModifiedDate )
@@ -16,12 +16,24 @@ import Model.Series ( formatModifiedDate )
 seriesTable :: TimeZone -> Colonnade Headed Series (Cell site)
 seriesTable localTimeZone = mconcat
   [ headed "Title"               (textCell . seriesTitle)
-  , headed "Source"              (textCell . maybe "" id . seriesSourceName)
+  , headed "Source"              mkSourceCell
   , headed "Publication status"  (textCell . tshow . seriesPubStatus)
   , headed "Reading status"      (textCell . tshow . seriesReadingStatus)
   , headed "Issues read"         (textCell . tshow . seriesIssuesRead)
   , headed "Last modified"       (textCell . formatModifiedDate localTimeZone . seriesModified)
   ]
+
+
+mkSourceCell :: Series -> Cell site
+mkSourceCell series = case (seriesSourceName series, seriesSourceUrl series) of
+  (Just sourceName, Just sourceUrl) -> mkA sourceName sourceUrl
+  (Just sourceName, Nothing       ) -> textCell sourceName
+  (Nothing,         Just sourceUrl) -> mkA "source"   sourceUrl
+  (Nothing,         Nothing       ) -> textCell ""
+
+  where
+    mkA :: Text -> Text -> Cell site
+    mkA sourceName' sourceUrl' = cell [whamlet|<a href=#{sourceUrl'}>#{sourceName'}|]
 
 
 getSeriesListR :: Handler Html
